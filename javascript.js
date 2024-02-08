f30b35844517ef433a88102b400c4561fdb58356
rdesmond21 <128184517+Dezz-x@users.noreply.github.com>
2024-02-07 19:56:36 -0500

    Fixed some bugs
    Started work on the playGame function
    Game is now iterating over the round 5 times
    computerSelection is working great
    playerSelection is working great
    Need to add score tracker as final step

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 //Generating random pick for the computer
-const picks = ["Rock", "Paper", "Scissors"];
+/*const picks = ["Rock", "Paper", "Scissors"];
 const computerChoice = picks[Math.floor(Math.random() * picks.length)];
 
 function getComputerChoice(){
@@ -8,46 +8,81 @@ function getComputerChoice(){
 
 //Variables for both player and computer selections
 const computerSelection = getComputerChoice();
+
 const playerSelection = prompt("Choose your weapon: Rock, Paper, or Scissors?");
+console.log("Player selects: " + playerSelection)
 
 //One round function that logs a score to the console
 function playRound(playerSelection, computerSelection) {
-
-    let playerScore = 0;
-    let computerScore = 0;
     
     if (playerSelection == computerSelection) {
         alert("It's a tie!");
     }
     else if (playerSelection == "Rock" && computerSelection == "Scissors") {
-        playerScore++
         alert("You win! Rock beats scissors");
     }
     else if (playerSelection == "Rock" && computerSelection == "Paper") {
-        computerScore++
         alert("You lose :( Paper beats rock");
     }
     else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-        playerScore++
         alert("You win! Scissors beats paper");
     }
     else if (playerSelection == "Scissors" && computerSelection == "Rock"){
-        computerScore++
         alert("You lose :( Rock beats scissors");
     }
     else if (playerSelection == "Paper" && computerSelection == "Rock"){
-        playerScore++
         alert("You win! Paper beats rock");
     }
     else if (playerSelection == "Paper" && computerSelection == "Scissors"){
-        computerScore++
         alert("You lose :( Scissors beats paper");
     }
-    console.log("Player Score: " + playerScore);
-    console.log("CPU Score: " + computerScore);
-};
-console.log(playRound(playerSelection,computerSelection));
+} 
+*/
 
 //Best of 5 function that loops through the playRound function 5 times
+let playerScore = 0;
+let computerScore = 0;
+
 function playGame(){
-};
+    for (let i = 0; i < 5; i++){
+        
+        const picks = ["Rock", "Paper", "Scissors"];
+        const computerChoice = picks[Math.floor(Math.random() * picks.length)];
+
+        function getComputerChoice(){
+            return computerChoice;
+        }
+        
+        const computerSelection = getComputerChoice();
+        console.log("CPU selects: " + computerSelection);
+        const playerSelection = prompt("Choose your weapon: Rock, Paper, or Scissors?");
+        console.log("Player selects: " + playerSelection);
+
+        function playRound(playerSelection, computerSelection) {
+    
+            if (playerSelection == computerSelection) {
+                alert("It's a tie!");
+            }
+            else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+                alert("You win! Rock beats scissors");
+            }
+            else if (playerSelection == "Rock" && computerSelection == "Paper") {
+                alert("You lose :( Paper beats rock");
+            }
+            else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+                alert("You win! Scissors beats paper");
+            }
+            else if (playerSelection == "Scissors" && computerSelection == "Rock"){
+                alert("You lose :( Rock beats scissors");
+            }
+            else if (playerSelection == "Paper" && computerSelection == "Rock"){
+                alert("You win! Paper beats rock");
+            }
+            else if (playerSelection == "Paper" && computerSelection == "Scissors"){
+                alert("You lose :( Scissors beats paper");
+            }
+        } playRound(playerSelection, computerSelection); 
+
+    }
+}
+playGame();
